c7c3ba94967b3a5dd67a48d212dff334c1e5e555
sudheerj <jsudheer50@gmail.com>
2018-03-14 09:51:51 +0800

    Move absolute imports to top

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import 'primereact/resources/primereact.min.css';
+import 'primereact/resources/themes/omega/theme.css';
+import 'font-awesome/css/font-awesome.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
@@ -5,9 +9,7 @@ import './index.css';
 import Search from './views/Search';
 import Github from './views/Github';
 
-import 'primereact/resources/primereact.min.css';
-import 'primereact/resources/themes/omega/theme.css';
-import 'font-awesome/css/font-awesome.css';
+
 
 ReactDOM.render(
     <Router>
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: `${__dirname}/src/index.js`,
   output: {
     path: `${__dirname}/build`,
-    publicPath: '/build/',
+    publicPath: '/react-github/build/',
     filename: 'bundle.js',
   },
 
